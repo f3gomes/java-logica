7218bb98591f7cb82435bdfa1b91806c499500e7
f3gomes <fgomesdeluna@gmail.com>
2025-05-04 17:01:00 -0300

    incluir tela inicial

---
+++ APS/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+      TelaInicial home = new TelaInicial();
+
+      home.atulizaMensagemInicial();
+      home.exibirMensagemInicial();
+  }
+}
---
+++ APS/TelaInicial.java
@@ -0,0 +1,11 @@
+public class TelaInicial {
+  private String mensagem;
+
+  public void exibirMensagemInicial() {
+    System.out.println(this.mensagem);
+  }
+
+  public void atulizaMensagemInicial() {
+    this.mensagem = "Seja bem-vindo ao nosso programa interativo!\n\n Este espaço foi criado para estimular a reflexão e o aprendizado sobre temas fundamentais relacionados à igualdade de gênero e à luta das mulheres na sociedade. Aqui, você terá a oportunidade de aplicar seus conhecimentos de lógica de programação para desenvolver soluções que conscientizem sobre questões urgentes, como o machismo estrutural, a violência doméstica, a luta pelos direitos das mulheres e as disparidades de gênero no mercado de trabalho.\n\n Ao responder ao questionário, lembre-se de que o objetivo é aprofundar seu entendimento e engajamento com esses temas, enquanto também exercita suas habilidades de programação. Sua contribuição é fundamental para ampliar a conscientização e promover a mudança social.\n\n Vamos juntos nessa jornada de aprendizado e transformação!";
+  }
+}
